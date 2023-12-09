197425de457c9895c25a4bb5d240615eabff71f1
Daniel Gauthier <daniel.m.gauthier@icloud.com>
2023-12-08 20:42:40 -0500

    Day 6.

--- Sources/AdventOfCode2023/Day6.swift
+++ Sources/AdventOfCode2023/Day6.swift
@@ -1,14 +1,59 @@
 import Foundation
 import Algorithms
 
+struct Race {
+    var time: Int
+    var distance: Int
+}
+
 struct Day6: Runnable {
     var input: String
     
     func partOne() -> String {
-        fatalError()
+        let lines = input.lines
+        let times = Array(lines[0].splitAndTrim(by: " ").dropFirst())
+        let distances = Array(lines[1].splitAndTrim(by: " ").dropFirst())
+        
+        var races: [Race] = []
+        for i in 0..<times.count {
+            races.append(Race(time: times[i].toInt(), distance: distances[i].toInt()))
+        }
+        
+        return races.map { race in
+            var winConditions = 0
+            for pushTime in 1..<race.time {
+                let distanceTraveled = (race.time - pushTime) * pushTime
+                if distanceTraveled > race.distance {
+                    winConditions += 1
+                }
+            }
+            return winConditions
+        }
+        .reduce(1) { partialResult, count in
+            partialResult * count
+        }
+        .description
     }
     
     func partTwo() -> String {
-        fatalError()
+        let lines = input.lines
+        let time = Double(lines[0]
+            .splitAndTrim(by: ":")[1]
+            .splitAndTrim(by: " ")
+            .reduce(into: "") { $0 = "\($0)\($1)" }
+            .toInt()
+        )
+        
+        let distance = Double(lines[1]
+            .splitAndTrim(by: ":")[1]
+            .splitAndTrim(by: " ")
+            .reduce(into: "") { $0 = "\($0)\($1)" }
+            .toInt()
+        )
+        
+        let point1 = (time - sqrt((-time * -time) - 4 * distance)) / 2
+        let point2 = (time + sqrt((-time * -time) - 4 * distance)) / 2
+        
+        return (Int(point2) - Int(ceil(point1)) + 1).description
     }
 }
--- Sources/AdventOfCode2023/Runnable.swift
+++ Sources/AdventOfCode2023/Runnable.swift
@@ -1,3 +1,5 @@
+import Foundation
+
 protocol Runnable {
     var input: String { get }
     func run()
@@ -7,24 +9,46 @@ protocol Runnable {
 
 extension Runnable {
     func run() {
-        printResult(name: "Part 1", result: partOne())
+        let start1 = CFAbsoluteTimeGetCurrent()
+        let part1Result = partOne()
+        let time1 = CFAbsoluteTimeGetCurrent() - start1
+        
+        printResult(name: "Part 1", result: part1Result, time: time1)
         print("")
-        printResult(name: "Part 2", result: partTwo())
+        
+        let start2 = CFAbsoluteTimeGetCurrent()
+        let part2Result = partTwo()
+        let time2 = CFAbsoluteTimeGetCurrent() - start2
+        
+        
+        printResult(name: "Part 2", result: part2Result, time: time2)
         print("\n")
     }
     
-    private func printResult(name: String, result: String) {
+    private func printResult(name: String, result: String, time: CFAbsoluteTime) {
         print("------\(name)------")
         print("|                |")
-        let result = result
         let length = result.count
         let space = 10 + name.count - length
         let leftSpace = space / 2
         let rightSpace = space - leftSpace
+        
+        let timeString = String(format: "(%.1fms)", time * 1000)
+        let timeLength = timeString.count
+        let timeSpace = 10 + name.count - timeLength
+        let leftTimeSpace = timeSpace / 2
+        let rightTimeSpace = timeSpace - leftTimeSpace
+        
         print("|", terminator: "")
         print(String(repeating: " ", count: leftSpace), terminator: "")
         print(result, terminator: "")
         print(String(repeating: " ", count: rightSpace), terminator: "|\n")
+        
+        print("|", terminator: "")
+        print(String(repeating: " ", count: leftTimeSpace), terminator: "")
+        print(timeString, terminator: "")
+        print(String(repeating: " ", count: rightTimeSpace), terminator: "|\n")
+        
         print("|                |")
         print("|-----\(String(repeating: "-", count: name.count))-----|")
         print("|\(String(repeating: " ", count: name.count + 10))|")
